6c9f7140c02591813a485991621a9b0e3c586731
Andrea-Calligari <andrea.calligari26@gmail.com>
2024-06-12 16:04:02 +0200

    ADDED: api call and gotten all results from back-end + startin appcard component

---
+++ src/components/AppCard.vue
@@ -0,0 +1,36 @@
+<script >
+import axios from 'axios';
+export default {
+  data(){
+    return{
+      projects : [],
+    }
+  },
+  methods: {
+    fetchProjects(){
+      axios.get('http://127.0.0.1:8000/api/projects').then((res) => {
+        this.projects = res.data.projects
+        // console.log(this.projects) 
+      })
+    }
+  },
+  created(){
+    this.fetchProjects()
+  }
+}
+</script>
+
+<template>
+ <h1>I miei Progetti </h1>
+
+  <ul>
+    <li v-for="project in projects" :key="project.id">
+      <h3> Titolo :{{ project.project_name }}</h3>
+      <p> Descrizione: {{ project.description }}</p>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+
+</style>
